a4359cccde5deb5ceef6b7da16c479c1153e08a3
Elizxo <108903373+Elizxo@users.noreply.github.com>
2025-09-28 00:07:04 -0400

    Create Chapter0.R

---
+++ Chapter0.R
@@ -0,0 +1,87 @@
+### R STUDIO ####################################################
+
+# To run code, click on a line, or highlight multiple lines, and click "run."
+# You can also type ctrl + enter. 
+
+# R is case-sensitive
+
+### Basic Math Operations ######################################################
+
+3 + 7 # addition 
+
+11 - 28 # subtraction 
+
+4 * 3 # multiplication 
+
+9 / -8 # division 
+
+3**2 # exponents 
+3^2
+
+sqrt(77) # square root 
+
+8^(1/3) # cube root 
+
+factorial(4) # factorial = 4!
+
+# order of operations: 
+
+m = ((3 - 187) * 14 - (-44)) / 3.33 
+
+print(m) # prints the value of m in the console 
+
+n = 3 - 187 * 14 - -44 / 3.33
+
+print(n)
+
+### Creating Objects ########################################################### 
+
+# To create an object, you first type the name you want the object to be and 
+# then assign it a value by using either <- or = 
+
+a <- 3 # numerical objects 
+
+A = 5
+
+a + A
+
+color = "Red" # character/string objects 
+
+shape = "square"
+
+# A vector is an object that contains a list of values. 
+
+my_numbers <- c(3, 5.55, -6, 12.0003) # vector of numerical variables 
+print(my_numbers)
+
+friends <- c("Matt", "Karen", "Foggy", "Claire") # vector of character variables 
+print(friends) 
+ 
+
+### Practice Sheet #################################################
+
+#2. Solve: 
+
+((4/3)^3 - 8.44 + sqrt(98))/-7 
+
+#3. Make an object:
+
+B <- 33.978
+
+#4. Make an object: 
+
+C <- factorial(7)
+
+#5. Solve:
+
+B + 2*C 
+
+#6. Make a vector:
+
+numbers <- c(64, 35, 2)
+print(numbers)
+
+#7. Make a vector: 
+
+family <- c("Jessie", "Brock", "James", "Misty")
+print(family)
